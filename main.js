dc6ad523d9d65a47a93516fc9618951e61789edb
Raman Boddula <ramanboddula@gofynd.com>
2025-01-26 00:58:27 +0530

    first commit

--- main.js
+++ main.js
@@ -8,6 +8,40 @@ window.addEventListener("scroll", function()
     parallax.style.backgroundPositionX = offset*(-0.3)-100 + "px";
 })
 
+let hasCelebrated = false;
+
+document.addEventListener('scroll', () => {
+  const firstPage = document.getElementById('home-img-lg');
+  const firstPageBottom = firstPage.getBoundingClientRect().bottom;
+
+  if (!hasCelebrated && firstPageBottom <= window.innerHeight + 50) {
+    let i = 0;
+    while (i <= 10) {
+      celebrate();
+      hasCelebrated = true;
+      i++;
+    }
+  }
+});
+function celebrate() {
+  confetti({
+    spread: 70,
+    startVelocity: 30,
+    elementCount: 1000,
+    dragFriction: 0.1,
+    duration: 3000,
+    colors: ['#bb0000', '#ffffff', '#00bb00', '#00aaff', '#ffbb00']
+  });
+}
+const addToCalender = () => {
+  celebrate();
+  setTimeout(() => {
+    window.open(
+      "https://www.google.com/calendar/render?action=TEMPLATE&text=Wedding+of+Raman+and+Gayatri&dates=20250212T133000Z/20250212T173000Z&details=Join+us+to+celebrate+the+wedding+of+Raman+and+Gayatri&location=Vaaraahi+Banquets+and+Conference+Center,+Level+3,+SLN+Terminus,+Jayabheri+Enclave,+Gachibowli,+Hyderabad,+Telangana+500032",
+      "_blank"
+    );
+  }, 1000);
+};
 
 window.addEventListener("scroll", function()
 {
